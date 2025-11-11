a98337e05f6fb4a39e192f9572e81d2d5726ad83
EderMoros3 <edermorosrodero@gmail.com>
2025-11-11 12:50:22 +0100

    feat: add characteristics section to About page with icons and descriptions

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -37,6 +37,91 @@ export default function About() {
       </section>
 
       <section className="caracteristicas">
+        <div className="caracteristicas__content">
+          <div className="caracteristicas__info">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-stars"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M17.8 19.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z" />
+              <path d="M6.2 19.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z" />
+              <path d="M12 9.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z" />
+            </svg>
+            <h3>Creatividad</h3>
+            <p>
+              Innovamos constantemente para ofrecerte las últimas tendencias y
+              estilos personalizados.
+            </p>
+          </div>
+          <div className="caracteristicas__info">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-plant-2"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M2 9a10 10 0 1 0 20 0" />
+              <path d="M12 19a10 10 0 0 1 10 -10" />
+              <path d="M2 9a10 10 0 0 1 10 10" />
+              <path d="M12 4a9.7 9.7 0 0 1 2.99 7.5" />
+              <path d="M9.01 11.5a9.7 9.7 0 0 1 2.99 -7.5" />
+            </svg>
+            <h3>Bienestar</h3>
+            <p>
+              Tu comodidad y satisfacción son nuestra mámima prioridad en cada
+              visita.
+            </p>
+          </div>
+          <div className="caracteristicas__info">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-circle-dashed-check"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M8.56 3.69a9 9 0 0 0 -2.92 1.95" />
+              <path d="M3.69 8.56a9 9 0 0 0 -.69 3.44" />
+              <path d="M3.69 15.44a9 9 0 0 0 1.95 2.92" />
+              <path d="M8.56 20.31a9 9 0 0 0 3.44 .69" />
+              <path d="M15.44 20.31a9 9 0 0 0 2.92 -1.95" />
+              <path d="M20.31 15.44a9 9 0 0 0 .69 -3.44" />
+              <path d="M20.31 8.56a9 9 0 0 0 -1.95 -2.92" />
+              <path d="M15.44 3.69a9 9 0 0 0 -3.44 -.69" />
+              <path d="M9 12l2 2l4 -4" />
+            </svg>
+            <h3>Calidad</h3>
+            <p>
+              Utilizamos solo los mejores productos y técnicas para garantizar
+              resultados excepcionales.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="team-members">
         
       </section>
     </div>
