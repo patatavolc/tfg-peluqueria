7522d84bb14ca5c44b3dae99a0ccb5ebfd25325c
EderMoros3 <edermorosrodero@gmail.com>
2025-12-04 11:51:12 +0100

    feat: ContactForm component + db schema in excell

--- frontend/src/components/ContactForm/ContactForm.tsx
+++ frontend/src/components/ContactForm/ContactForm.tsx
@@ -1,26 +1,31 @@
 import "./ContactForm.css";
-
+import Button from "../Button/Button.tsx";
 interface ContactFormProps {
   title : string;
 }
 
 export default function ContactForm({title}:ContactFormProps) {
   return (
-    <div>
+    <form>
       <h2>{title}</h2>
       <div className="contact-form__labels">
         <label htmlFor="name">Nombre</label>
-        <input type="text" name="name" id="name" placeholder="Tu nombre" />
+        <input type="text" name="name" id="name" placeholder="Tu nombre" required/>
       </div>
       <div className="contact-form__labels">
         <label htmlFor="email">Correo electronico</label>
-        <input type="email" name="email" id="email" placeholder="tu@email.com" />
+        <input type="email" name="email" id="email" placeholder="tu@email.com" required />
       </div>
       <div className="contact-form__labels">
         <label htmlFor="asunto">Asunto</label>
-        <input type="text" name="asunto" id="asunto" placeholder="Asunto del mensaje" />
+        <input type="text" name="asunto" id="asunto" placeholder="Asunto del mensaje" required/>
       </div>
-    </div>
+      <div className="contact-form__labels message">
+        <label htmlFor="message">Mensaje</label>
+        <textarea name="massage" id="nsg" placeholder="Escribe tu mensaje aqui..." required></textarea>
+      </div>
+      <Button text="Enviar" variant="primary" />
+    </form>
   )
 };
 
--- frontend/src/pages/Contact/Contact.tsx
+++ frontend/src/pages/Contact/Contact.tsx
@@ -3,7 +3,7 @@ import "./Contact.css";
 import TitleAndSubtitle from "../../components/TitleAndSubtitle/TitleAndSubtitle.tsx";
 import ContactInfo from "../../components/ContactInfo/ContactInfo.tsx";
 import MapEmbed from "../../components/MapEmbed/MapEmbed.tsx";
-
+import ContactForm from "../../components/ContactForm/ContactForm.tsx";
 export default function Contact() {
   return (
     <Layout>
@@ -22,6 +22,7 @@ export default function Contact() {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4297.692127045282!2d-0.8862912146737787!3d41.677717601440406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd59149fcb56d5a5%3A0x57068ad103084f62!2sEn%20tu%20fiesta%20me%20qued%C3%A9!5e1!3m2!1ses!2ses!4v1763637142766!5m2!1ses!2ses"
         title="En tu fiesta me quede "
       />
+      <ContactForm title="Envianos un mensaje"/>
     </Layout>
   );
 }
