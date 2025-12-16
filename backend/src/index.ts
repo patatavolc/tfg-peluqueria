4caf82b34552064da117d2cd5ff149e46075e9bb
EderMoros3 <edermorosrodero@gmail.com>
2025-12-16 11:59:40 +0100

    backend-feat: crear mainRouter + add ruta a index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { pool, connectDB } from "./config/database.ts";
+import mainRouter from "./routes/mainRouter.ts";
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", mainRouter);
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando");
