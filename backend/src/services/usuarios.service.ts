458803eeea02afaf8b6a5f5fbe35d9f382621ef4
patatavolc <edermorosrodero@gmail.com>
2026-01-08 10:04:52 +0100

    backend-feat: add getAllUsers, getUserById, getUserByEmail and getUserByTelefono services

--- backend/src/services/usuarios.service.ts
+++ backend/src/services/usuarios.service.ts
@@ -25,3 +25,29 @@ export const newUser = async (data: Usuario) => {
 
   return result.rows[0];
 };
+
+export const getAllUsers = async () => {
+  const query = `SELECT id, nombre, email, telefono, rol, creado_en FROM usuarios ORDER BY creadoen DESC`;
+  const result = await pool.query(query);
+  return result.rows[0];
+};
+
+export const getUserById = async (id: number) => {
+  const query = `SELECT id, nombre, email, telefono, rol, creado_en FROM usuarios WHERE id = $1`;
+  const result = await pool.query(query, [id]);
+  return result.rows[0];
+};
+
+export const getUserByEmail = async (email: string) => {
+  const query =
+    "SELECT id, nombre, email, telefono, rol, creado_en FROM usuarios WHERE email = $1";
+  const result = await pool.query(query, [email]);
+  return result.rows[0];
+};
+
+export const getUserByTelefono = async (telefono: string) => {
+  const query =
+    "SELECT id, nombre, email, telefono, rol, creado_en FROM usuarios WHERE telefono = $1";
+  const result = await pool.query(query, [telefono]);
+  return result.rows[0];
+};
