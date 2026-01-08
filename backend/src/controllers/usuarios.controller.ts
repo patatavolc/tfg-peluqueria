19a898662d79c0df7b08d59e97d9066f53af4123
patatavolc <edermorosrodero@gmail.com>
2026-01-08 09:52:55 +0100

    backend-feat: add getUserById, getUserByTelefono and updateUser controllers

--- backend/src/controllers/usuarios.controller.ts
+++ backend/src/controllers/usuarios.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-import { newUser, getAllUsers } from "../services/usuarios.service.tsx";
+import {
+  newUser,
+  getAllUsers,
+  getUserById,
+  getUserByTelefono,
+  updateUser,
+} from "../services/usuarios.service.tsx";
+import { error } from "node:console";
 
 export const createUsuario = async (
   req: Request,
@@ -33,4 +40,61 @@ export const getUsuarios = async (
   }
 };
 
+export const getUsuarioById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(parseInt(id));
 
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsuarioByTelefono = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { telefono } = req.params;
+    const user = await getUserByTelefono(telefono);
+
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUsuario = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+
+    const updatedUser = await updateUser(parseInt(id), data);
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
