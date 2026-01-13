4e232fecd5c4524c2d0ee512705d95e640fb3a68
patatavolc <edermorosrodero@gmail.com>
2026-01-13 11:13:21 +0100

    backend-feat: create getServicios + getServicioById controllers

--- backend/src/controllers/servicios.controller.ts
+++ backend/src/controllers/servicios.controller.ts
@@ -5,6 +5,7 @@ import {
   getServiceById,
   updateService,
 } from "../services/servicios.service";
+import { error } from "node:console";
 
 export const createServicio = async (
   req: Request,
@@ -21,16 +22,45 @@ export const createServicio = async (
       !data.duracion_minutos ||
       data.categoria
     ) {
-      return res
-        .status(400)
-        .json({
-          error:
-            "Faltan datos obligatoios (titulo, precio, duracion, categoria",
-        });
+      return res.status(400).json({
+        error: "Faltan datos obligatoios (titulo, precio, duracion, categoria",
+      });
     }
 
     const createdService = await newService(data);
     res.status(201).json(createdService);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getServicios = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const services = await getAllServices();
+    res.status(200).json(services);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getServicioById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const service = await getServiceById(parseInt(id as string));
+
+    if (!service) {
+      return res.status(404).json({ error: "Servicio no encontrado" });
+    }
+
+    res.status(200).json(service);
   } catch (error) {
     next(error);
   }
